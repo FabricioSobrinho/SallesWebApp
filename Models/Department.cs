0e5a95e2609ae967f2c1e34f864760519c2689da
FabricioSobrinho <fabriciomails3@gmail.com>
2024-02-23 14:51:35 -0400

    Refactor: correct nulable atribute Name

--- Models/Department.cs
+++ Models/Department.cs
@@ -1,8 +1,35 @@
-﻿namespace SallesWebApp.Models
+﻿using System.Collections.Generic;
+
+namespace SallesWebApp.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department() { }
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public void RemoveSeller(Seller seller)
+        {
+            Sellers.Remove(seller);
+        }
+
+        public double TotalSalles(DateTime initial, DateTime final)
+        {
+            var totalSalles = Sellers.Sum(seller => seller.TotalSalles(initial, final));
+
+            return totalSalles;
+        }
     }
 }
