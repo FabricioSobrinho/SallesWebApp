da24db58c898e2abcfc5a46459bc71443bc7d65a
FabricioSobrinho <fabriciomails3@gmail.com>
2024-02-23 14:49:37 -0400

    Feat: add new entities migrations

--- Migrations/20240223180237_OtherEntities.Designer.cs
+++ Migrations/20240223180237_OtherEntities.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SallesWebApp.Data;
 
@@ -10,9 +11,11 @@ using SallesWebApp.Data;
 namespace SallesWebApp.Migrations
 {
     [DbContext(typeof(SallesWebAppContext))]
-    partial class SallesWebAppContextModelSnapshot : ModelSnapshot
+    [Migration("20240223180237_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
