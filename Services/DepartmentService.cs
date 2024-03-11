ce0d0d1330ea240daf7193f427bb535d532f238a
FabricioSobrinho <fabriciomails3@gmail.com>
2024-03-11 09:21:48 -0400

    Feat: add department services

---
+++ Services/DepartmentService.cs
@@ -0,0 +1,20 @@
+﻿using SallesWebApp.Data;
+using SallesWebApp.Models;
+
+namespace SallesWebApp.Services
+{
+	public class DepartmentService
+	{
+		private readonly SallesWebAppContext _context;
+
+		public DepartmentService(SallesWebAppContext context)
+		{
+			_context = context;
+		}
+
+		public List<Department> FindAll()
+		{
+			return _context.Department.OrderBy(x => x.Name).ToList();
+		}
+	}
+}
