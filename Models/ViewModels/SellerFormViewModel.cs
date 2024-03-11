ed6350a82da39e03cccf005895fd716565cf1430
FabricioSobrinho <fabriciomails3@gmail.com>
2024-03-11 09:21:24 -0400

    Feat: create seller view models

---
+++ Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+namespace SallesWebApp.Models.ViewModels
+{
+	public class SellerFormViewModel
+	{
+        public Seller? Seller { get; set; }
+        public ICollection<Department>? Departments { get; set; }
+    }
+}
