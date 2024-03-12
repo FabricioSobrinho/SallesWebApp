589ea63750d02805e8573eb90ca0f659d1fb4362
FabricioSobrinho <fabriciomails3@gmail.com>
2024-03-12 09:21:23 -0400

    Feat: create Edit method

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -2,74 +2,50 @@
 using SallesWebApp.Models;
 using SallesWebApp.Models.ViewModels;
 using SallesWebApp.Services;
+using SallesWebApp.Services.Exceptions;
 
 namespace SallesWebApp.Controllers
 {
-    public class SellersController : Controller
-    {
-        private readonly SellerService _sellerService;
-        private readonly DepartmentService _departmentService;
+	public class SellersController : Controller
+	{
+		private readonly SellerService _sellerService;
+		private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService, DepartmentService departmentService)
-        {
-            _sellerService = sellerService;
-            _departmentService = departmentService;
-        }
-
-        public IActionResult Index()
-        {
-            var list = _sellerService.FindAll();
-            return View(list);
-        }
-
-        public IActionResult Create()
-        {
-            var departments = _departmentService.FindAll();
-            var viewModel = new SellerFormViewModel { Departments = departments};
-            return View(viewModel);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller seller)
-        {
-            _sellerService.Insert(seller);
-            return RedirectToAction(nameof(Index));
-        }
-
-        public IActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var seller = _sellerService.FindById(id.Value);
-
-			if (seller == null)
-			{
-				return NotFound();
-			}
-
-            return View(seller);
+		public SellersController(SellerService sellerService, DepartmentService departmentService)
+		{
+			_sellerService = sellerService;
+			_departmentService = departmentService;
 		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Delete(int id)
-        {
-            _sellerService.Remove(id);
-            return RedirectToAction(nameof(Index));
-        }
+		public IActionResult Index()
+		{
+			var list = _sellerService.FindAll();
+			return View(list);
+		}
 
-        public IActionResult Details(int id)
-        {
+		public IActionResult Create()
+		{
+			var departments = _departmentService.FindAll();
+			var viewModel = new SellerFormViewModel(departments);
+			return View(viewModel);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Seller seller)
+		{
+			_sellerService.Insert(seller);
+			return RedirectToAction(nameof(Index));
+		}
+
+		public IActionResult Delete(int? id)
+		{
 			if (id == null)
 			{
 				return NotFound();
 			}
 
-            var seller = _sellerService.FindById(id);
+			var seller = _sellerService.FindById(id.Value);
 
 			if (seller == null)
 			{
@@ -78,5 +54,72 @@ namespace SallesWebApp.Controllers
 
 			return View(seller);
 		}
-    }
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Delete(int id)
+		{
+			_sellerService.Remove(id);
+			return RedirectToAction(nameof(Index));
+		}
+
+		public IActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var seller = _sellerService.FindById(id.Value);
+
+			if (seller == null)
+			{
+				return NotFound();
+			}
+
+			return View(seller);
+		}
+
+		public IActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+			var seller = _sellerService.FindById(id.Value);
+
+			if (seller == null)
+			{
+				return NotFound();
+			}
+
+			List<Department> departments = _departmentService.FindAll();
+			SellerFormViewModel viewModel = new SellerFormViewModel(seller, departments);
+
+			return View(viewModel);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(int id, Seller seller)
+		{
+			if (id != seller.Id)
+			{
+				return BadRequest();
+			}
+
+			try
+			{
+				_sellerService.Update(seller);
+				return RedirectToAction(nameof(Index));
+			} catch (NotFoundException)
+			{
+				return NotFound();
+			} catch (DbConcurrencyException)
+			{
+				return BadRequest();
+			}
+
+		}
+	}
 }
