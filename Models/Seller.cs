7995b52d3a3abbf5bc648c5a0d381a6df0d19861
FabricioSobrinho <fabriciomails3@gmail.com>
2024-03-18 14:10:00 -0400

    Feat: add data validation notations

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -7,8 +7,13 @@ namespace SallesWebApp.Models
 	public class Seller
 	{
 		public int Id { get; set; }
+
+		[Required(ErrorMessage = "{0} is a required field")]
+		[StringLength(60, MinimumLength = 10, ErrorMessage = "{0} size should be between {2} and {1} chars")]
 		public string? Name { get; set; }
+
 		[DataType(DataType.EmailAddress)]
+		[Required(ErrorMessage = "{0} is a required field")]
 		public string? Email { get; set; }
 
 		[Display(Name = "Birth date")]
@@ -18,6 +23,8 @@ namespace SallesWebApp.Models
 
 		[Display(Name = "Base salary")]
 		[DisplayFormat(DataFormatString = "{0:C2}")]
+		[Range(10, 1000000, ErrorMessage = "{0} must be between {1} and {2}")]
+		[Required(ErrorMessage = "{0} is a required field")]
 		public double BaseSalary { get; set; }
 		public Department? Department { get; set; }
 		public int DepartmentId { get; set; }
