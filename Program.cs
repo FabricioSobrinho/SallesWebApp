15b69c8e2da11388a1de33f5afdb62c5e44f4c20
FabricioSobrinho <fabriciomails3@gmail.com>
2024-03-18 13:22:34 -0400

    Feat: set default local to Us

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SallesWebApp.Data;
 using SallesWebApp.Services;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("SallesWebAppContext");
@@ -21,6 +23,16 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
+
+var enUs = new CultureInfo("en-US");
+var localeOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(enUs),
+    SupportedCultures = new List<CultureInfo> { enUs },
+    SupportedUICultures = new List<CultureInfo> { enUs }
+};
+
+app.UseRequestLocalization(localeOptions);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
