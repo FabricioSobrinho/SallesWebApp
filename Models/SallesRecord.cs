42865887c229d6000646def0d06e0c320ecce4ef
FabricioSobrinho <fabriciomails3@gmail.com>
2024-03-20 08:01:56 -0400

    Feat: format date and amount

--- Models/SallesRecord.cs
+++ Models/SallesRecord.cs
@@ -1,11 +1,19 @@
 ﻿using SallesWebApp.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace SallesWebApp.Models
 {
     public class SallesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [Display(Name = "Base salary")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [Range(10, 1000000, ErrorMessage = "{0} must be between {1} and {2}")]
+        [Required(ErrorMessage = "{0} is a required field")]
         public double Amount { get; set; }
         public SalleStatus Status { get; set; }
         public Seller? Seller { get; set; }
